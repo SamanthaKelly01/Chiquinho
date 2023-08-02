698377f1565ca1c85900982bb81a03a538ba4f0a
SamanthaKelly01 <samanthak2002@hotmail.com>
2023-08-02 15:07:58 -0300

    Configurações iniciais

---
+++ App.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 8000; // Obtém a porta configurada das variáveis de ambiente ou usa a porta padrão 8000
+
+app.get("", (req, res) => {
+    return res.json({
+        message: "Geral"
+    });
+});
+
+app.get("/cadastro", (req, res) => {
+    return res.json({
+        message: "Cadastro"
+    });
+});
+
+app.get("/*", (req, res) => {
+    const notFoundMessage = `
+        <html>
+        <body>
+            <p>Página não encontrada. Clique <a href="http://localhost:${port}">aqui</a> para voltar à página principal.</p>
+        </body>
+        </html>
+    `;
+    return res.status(404).send(notFoundMessage);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor Rodando = Chiquinho feliz! Acesse http://localhost:${port}`);
+});
